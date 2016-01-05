8bd6f857183ddce74ec920812d5c060f787fcb79
Dave Herzog <dherzog@dramafever.com>
2016-01-05 07:57:15 -0500

    add destructuring and fix README

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Run each example using node.js.
 $ node arguments.js
 ```
 
+For the ES6 examples, some need to be run in Firefox since certain features are not yet supported in Node 5.  The ES6 compatability table can be found at [https://kangax.github.io/compat-table/es6/](https://kangax.github.io/compat-table/es6/).
--- es6/syntax/destructuring.js
+++ es6/syntax/destructuring.js
@@ -1,3 +1,5 @@
+// Need to run in FF.  Not yet supported in Node 5.x
+
 function getAddress() {
   return {
     'city': 'Anytown',
@@ -15,3 +17,21 @@ let {city: c, state: s, zip: z} = getAddress();
 console.log('city: ' + c);
 console.log('state: ' + s);
 console.log('zip: ' + z);
+
+// destructure nested object
+let person = {
+  name: 'Aaron',
+  age: 35,
+  address: {
+    city: 'Philly',
+    state: 'PA',
+    zip: 11111
+  }
+}
+
+let {pname, page, address: {pcity, pstate, pzip}} = person;
+console.log('name: ' + pname);
+console.log('page: ' + page);
+console.log('city: ' + pcity);
+console.log('state: ' + pstate);
+console.log('zip: ' + pzip);
